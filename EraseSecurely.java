63a5ff4bc164a3fed3058f84d531bda3b41b456d
minnafeng <minnafeng02@gmail.com>
2022-09-08 00:21:28 -0400

    Update files

--- EraseSecurely.java
+++ EraseSecurely.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class T9Spelling {
+public class EraseSecurely {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt(); // number of ints
